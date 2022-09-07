ca81108e61116c8bf3598a73b9df986d3d304664
Aldo500 <111352050+Aldo500@users.noreply.github.com>
2022-09-06 23:38:04 -0500

    Add files via upload

---
+++ Gato/app/src/main/java/com/example/gatogerdoc/negocio/GatoNegocio.java
@@ -0,0 +1,41 @@
+package com.example.gatogerdoc.negocio;
+
+import android.widget.TextView;
+
+public class GatoNegocio<label>
+{
+    //lógica de negocio del gato
+    label = (TextView)findViewbyId(R.id.textViewres);
+    public GatoNegocio( )
+    {
+
+    }
+
+    public boolean isGanador1( int []resultado )
+    {
+        if boolean button1 == false {
+            if boolean button2 == false {
+                if boolean button3 == false}
+        }
+
+        label.setText("El ganador es x");
+    }
+    }
+
+    public boolean isGanador2( int []resultado )
+    {
+        if boolean button1 == true {
+            if boolean button2 == true {
+                if boolean button3 == true}
+    }
+
+        label.setText("El ganador es o");
+    }
+    }
+
+    public boolean isEmpate( int []resultado )
+    {
+
+    }
+
+}
